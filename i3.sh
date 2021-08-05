54191bf175ffdb295e5391e1eaca5381408f2e87
karng87 <karng87@gmail.com>
2021-08-05 17:03:46 +0900

    1st install script

---
+++ i3.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+sudo timedatectl set-ntp true
+sudo hwclock --systohc
+
+#sudo reflector -c Switzerland -a 12 --sort rate --save /etc/pacman.d/mirrorlist
+sudo pacman -Sy
+
+sudo firewall-cmd --add-port=1025-65535/tcp --permanent
+sudo firewall-cmd --add-port=1025-65535/udp --permanent
+sudo firewall-cmd --reload
+# sudo virsh net-autostart default
+
+sudo pacman -S xorg xorg-xinit i3-wm i3lock i3status i3blocks dmenu terminator firefox ttf-fira-mono ttf-hack ttf-fira-code terminus-font noto-fonts-cjk noto-fonts-emoji ttf-dejavu
+sudo cp /etc/X11/xinit/xinitrc ~/.xinitrc
+# delete line and add exec i3  at the last of ~/.xinitrc  
+
+#sudo pacman -S simplescreenrecorder arc-gtk-theme arc-icon-theme obs-studio vlc dina-font tamsyn-font bdf-unifont ttf-bitstream-vera ttf-croscore ttf-dejavu ttf-droid gnu-free-fonts ttf-ibm-plex ttf-liberation ttf-linux-libertine noto-fonts ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family ttf-anonymous-pro ttf-cascadia-code ttf-fantasque-sans-mono ttf-fira-mono ttf-hack ttf-fira-code ttf-inconsolata ttf-jetbrains-mono ttf-monofur adobe-source-code-pro-fonts cantarell-fonts inter-font ttf-opensans gentium-plus-font ttf-junicode adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk noto-fonts-emoji
+
+# sudo flatpak install -y spotify
+# sudo flatpak install -y kdenlive
+
+sudo systemctl enable gdm
+/bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
+sleep 5
+sudo reboot
