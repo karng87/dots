f41fab493fa30e95a18e72d667c52a13cba13af7
karng87 <karng87@gmail.com>
2021-08-15 12:22:57 +0900

    asus

--- scripts/func.sh
+++ scripts/func.sh
@@ -28,15 +28,14 @@ function printDic(){
     clear
     echo "#######- ${W2} -#######" && \
     curl -s -A 'Mozilla/4.0' https://dic.daum.net/search.do?q=${W2} |\
-    sed -E 's/[^>]+|>([^<]+<?)/\1/g' |\
+    sed -E 's/[^>]+|([^<]+)/\1/g' |\
     sed -En '/완료/,/영영사전 더보기/p' |\
-    sed -E '/^>$/d' |\
-    sed -E '/^[[>a-z]/!d' |\
-    awk '!x[$0]++' |\
+    sed -E '/^>+$/d' |\
     sed -E 's/<|>//g' |\
+    sed -E '/^듣기$|^영영사전|^영어사전/d' |\
+    awk '!x[$0]++' |\
     sed -E 's/(\.)(\S)/\1 \2/' |\
-    sed -E 's/^[0-9]/\t&/' 
-    #sed -E -n '1,+25p'
+    sed -E 's/^[0-9]/\t&/'
     echo "================ END ==================="
 }
- 
+printDic $1
--- scripts/getWd.sh
+++ scripts/getWd.sh
@@ -4,11 +4,10 @@ function getWd(){
   local W=${1%%.*}
   local W1=${W##*/}
   local W2=${W1%%-*}
-  wd=$( curl -s -A 'Mozilla/4.0' https://dic.daum.net/search.do?q=${W2}|\
+  curl -s -A 'Mozilla/4.0' https://dic.daum.net/search.do?q=${W2}|\
     awk /wordid/ |\
     awk 'NR==1{print $0}' |\
-    sed -E 's/.*>(\w+).*/\1/' )
+    sed -E 's/.*>(\w+).*/\1/'
 }
-
-getWd $1
-echo wd
+wd=$(getWd $1)
+echo $wd
