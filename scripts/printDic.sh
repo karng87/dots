21f8852d02bdfbc824afdd3899f276f4aa3d7770
karng87 <karng87@gmail.com>
2021-08-16 20:22:48 +0900

    asus

--- scripts/printDic.sh
+++ scripts/printDic.sh
@@ -6,14 +6,32 @@ function printDic(){
     clear
     echo "#######- ${W2} -#######" && \
     curl -s -A 'Mozilla/4.0' https://dic.daum.net/search.do?q=${W2} |\
-    sed -En '16p' |\
+    sed -En '/완료/,/내 단어장에 저장/p' |\
+    sed -E 's/<\/div>/\n/' |\
+
+    sed -E 's/(num_search)/>\1</' |\
+    sed -E 's/(sub_txt)/>\1</' |\
+    sed -E 's/(txt_search)/>\1</' |\
+
+      #sed -E 's/[^>"]+|"([^"]+)"|([^<"]+)/\1\2/g' |\
+    sed -E 's/[^>]+|([^<]+)/\1/g' |\
+    sed -E '/^$|^>+$|^\s$/{d}' |\
+      # num_search  sub_txt txt_search
+    sed -E '/num_search|sub_txt|txt_search/!d' |\
+    sed -E 's/^>+txt_search>+([^>]*)>/\n\1/' |\
+    sed -E 's/>+txt_search>+//' |\
+    sed -E 's/>+num_search>+/\t/' |\
+    sed -E 's/>+sub_txt>+/\t/' 
+    #sed -E 's/^>/\t/' 
+    #sed -E '/^>num|^>sub|^>txt/! d'
+    #sed -E 's/>+|([^>]+)/\1/g' 
+
     # <meta name="twitter:title" content="Daum 사전" />
     #sed -E 's/([^=]+)|"([^"]+)"/[->\2<-]\n/g'
     #sed -E 's/([^=]+)|"([^"]+)"|(=)/[1->\1<-1][2->\2<-2][3->\3<-3]\n/'
     #sed -E 's/([^=]+)|"([^"]+)"|(=)/[1->\1<-1][2->\2<-2][3->\3<-3]\n/g'
     #sed -E 's/"([^"]+)"|(.+)/[1(\1)1] - [2(\2)2]\n/g'
     #sed -E 's/([^"]+)|"([^"]+)"|(.+)/[1(\1)1] - [2(\2)2] - [3(\3)3]\n/g'
-    sed -E 's/([^w]+)|([^"]+)/[1(\1)1] - [2(\2)2]\n/g'
         #sed -E 's/([^"]+)|([^"]+)|"/(1s)->\1<-(1e):(s2)->\2<-(2e)/g'
         #sed -E 's/([^"]+)|(["]+)|"/[->\2<-]/g'
         #sed -E 's/([^"]+)|(["]+)|"/[->\1<-]/g'
