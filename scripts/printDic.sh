4614428a2a4246ab96eba725db815afb7dc4dd06
karng87 <karng87@gmail.com>
2021-08-22 18:36:59 +0900

    sed Hold Go brangch

--- scripts/printDic.sh
+++ scripts/printDic.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 daum_id="https://dic.daum.net/word/view.do?wordid" #=ekw000146827
-dic_dir=~/Project/dots/scripts
+daum_tts="https://tts-dic.daum.net/read?lang=en&txt" #=
+audio_dir=~/Project/dic/mp3
 
 function printDic(){
   local W1=${1%%.*}
@@ -9,16 +10,47 @@ function printDic(){
   local W3=${W1%%_*}
   local W4=${W1%%-*}
 
-  local W=$( ./getWID.sh $W4 )
+  local W="$( ./getWID.sh $W4 )"
   local WD=${W##*>}
   local WID=${W%%>*}
+  local Contents_1=$( curl -s -A 'Mozilla/4.0' "${daum_id}=${WID}" )
+  local Contents_wd=$( sed -E \
+                          -e 's/(>[^<]+<)|data-audio data-url="([^"]+)"|./\1/g' \
+                        <<< $Contents_1 )
+  # txt_cleanword
+  #   num_mean txt_mean
+  #     txt_pronounce
+  #       data-audio data-url="h..
+
+  # inner_tit = 뜻
+  # tit_sort
+  #   txt_sort
+  #   txt_subword
+  #     num_mean txt_mean
+
+
+  # card_tit = families
+  # inner_tit = 관련어
+  # tit_word
+  #   txt_emph1
+  #     mena_info 
+
+
+  # card_tit = examples
+  # inner_tit = 예문
+  # txt_word
+  #   txt_example txt_ex
+  #     data-audio data-url="h..
+  #     mean_example txt_ex
 
     clear
-    echo "#######- ${WD} -#######" && \
-    curl -s -A 'Mozilla/4.0' "${daum_id}=${WID}" |\
-    sed -E 's/>([^<]+)<|data-audio data-url="([^"]+)"|./\1\2/g' |\
-    sed -En '/^$/!p'
+    echo "======== ${WD} ========"
+    sed -En   '/txt_pronounce|txt_cleanword|data-audio data-url="http/{s/(>[^<]+\n?)|data-url=("[^"]+")|./\1\2/g;H;b}; ${g;p}' <<< $Contents_1
 
+    num=0
+
+  #for i in $Contents_snd;do curl -o ${audio_dir}/${WD}_$(( num++ )).mp3 $i 1>/dev/null; mpv $i 1>/dev/null 2>&1;sleep 1;done
     echo "================ END ==================="
 }
+
 printDic $1
